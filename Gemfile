1fa5dfa4be1e378aa7a3e39bacc2cfd220905e4c
YOUR NAME <YOUR@EMAIL.com>
2024-04-14 16:14:44 -0500

    update gem and cors

--- Gemfile
+++ Gemfile
@@ -6,7 +6,10 @@ ruby "3.3.0"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
 # Use postgresql as the database for Active Record
-gem "pg", "~> 1.1"
+if ENV['NETLIFY'] != 'true'
+  gem 'pg', '~> 1.5.6'
+end
+
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'example.com'  # Replace 'example.com' with the actual domain
+      resource '*',
+               headers: :any,
+               methods: [:get, :post, :put, :patch, :delete, :options, :head],
+               credentials: true
+    end
+  end
+  
